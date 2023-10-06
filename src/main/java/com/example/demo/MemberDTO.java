7407887d9cf4e8a3b7e1ff3046f701642369f30c
고동환\eurdr <eurdream98@naver.com>
2023-10-06 20:30:56 +0900

    02_handler_Method연습

---
+++ src/main/java/com/example/demo/MemberDTO.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+public class MemberDTO {
+    private String name;
+    private int price;
+    private int categoryCode;
+    private String orderableStatus;
+
+    public MemberDTO(String name, int price, int categoryCode, String orderableStatus) {
+        super();
+        this.name = name;
+        this.price = price;
+        this.categoryCode = categoryCode;
+        this.orderableStatus = orderableStatus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getOrderableStatus() {
+        return orderableStatus;
+    }
+
+    public void setOrderableStatus(String orderableStatus) {
+        this.orderableStatus = orderableStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberDTO{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", categoryCode=" + categoryCode +
+                ", orderableStatus='" + orderableStatus + '\'' +
+                '}';
+    }
+}
